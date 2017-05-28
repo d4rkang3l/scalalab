47b167c75716071a4a1c0347f857e8127641fe49
sterg <sterg@teiemt.gr>
2017-05-28 18:18:51 +0300

    minor update

--- src/main/java/scalaExec/Interpreter/GlobalValues.java
+++ src/main/java/scalaExec/Interpreter/GlobalValues.java
@@ -45,7 +45,7 @@ public class GlobalValues
 {
 
     
-    static public   String  scalalabBuildVersion = "23-May-17"; 
+    static public   String  scalalabBuildVersion = "28-May-17"; 
    
    static public boolean interruptcifor = false;
     static public ScalaInterpreterPane globalInterpreterPane;  // the interpreter pane object which is the main interface to the Scala interpreter
